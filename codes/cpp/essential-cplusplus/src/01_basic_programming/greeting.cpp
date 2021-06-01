dd02d0ba7eefd211ef1fb0d5eab02d5404bd699f
zhoujiagen <zhoujiagen@gmail.com>
2021-06-02 07:46:21 +0800

    Format code.

--- codes/cpp/essential-cplusplus/src/01_basic_programming/greeting.cpp
+++ codes/cpp/essential-cplusplus/src/01_basic_programming/greeting.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-int
-main (int argc, char **argv)
+int main(int argc, char **argv)
 {
   string user_name;
   cout << "Please enter your first name: ";
   cin >> user_name;
-  cout << '\n' << "Hello, " << user_name << " ... and goodbye!\n";
+  cout << '\n'
+       << "Hello, " << user_name << " ... and goodbye!\n";
 
   return 0;
 }
